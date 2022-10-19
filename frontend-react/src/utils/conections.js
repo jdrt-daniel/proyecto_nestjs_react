5a280df2233595d90e5b833dc72c91f3ccd456fd
Daniel <jdrt.1012@gmail.com>
2022-10-18 22:48:43 -0400

    frontend actualizado

--- frontend-react/src/utils/conections.js
+++ frontend-react/src/utils/conections.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /** NestJS */
-export const api = axios.create({ baseURL: "http://localhost:8080" });
+export const api = axios.create({ baseURL: "http://localhost:4000" });
 
 /** Laravel */
 export const apiTariquia = axios.create({ baseURL: "http://localhost:8000" });
